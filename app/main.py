87acaad3c6c0dfa599fdc314214c1b6bed4e2080
Siddharthachauhan <siddharthachauhan.connect@gmail.com>
2026-02-26 14:40:42 +0530

    add get notes endpoint

--- app/main.py
+++ app/main.py
@@ -20,3 +20,10 @@ def create_note(note: Note):
     note_id = len(notes) +1
     notes[note_id] = note
     return {"message": "Note created successfully!", "id": note_id, "note": note}
+
+@app.get("/get_note/{note_id}")
+def get_note(note_id:int):
+    if note_id not in notes:
+        return {"message": "Note not found!"}
+    return {"note": notes[note_id]}
+
