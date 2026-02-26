55ec8ca811cbac201687931a717fba5625f76031
Siddharthachauhan <siddharthachauhan.connect@gmail.com>
2026-02-26 12:55:16 +0530

    Add create note endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 app = FastAPI()
+
+notes ={}
+
+class Note(BaseModel):
+    title: str
+    content: str
+
 
 @app.get("/")
 def root():
     return {"message": "Notes API running!"}
+
+
+@app.post("/create_note")
+def create_note(note: Note):
+    note_id = len(notes) +1
+    notes[note_id] = note
+    return {"message": "Note created successfully!", "id": note_id, "note": note}
